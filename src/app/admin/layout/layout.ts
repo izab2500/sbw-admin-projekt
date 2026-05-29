e3fc478ed060d28c1554947e64a8a69767818204
ziba <ziba2500@student.miun.se>
2026-05-29 19:29:22 +0200

    Add logout to auth service and destroy httpOnly cookie

--- src/app/admin/layout/layout.ts
+++ src/app/admin/layout/layout.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-
+import { Component, inject} from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout { }
+export class Layout {
+  private readonly authService = inject(Auth);
+  private readonly router = inject(Router);
+
+  logoutAdmin() {
+    this.authService.logoutAdmin().subscribe({
+      next: (res) => {
+        console.log('Logga ut:', res.message);
+        this.router.navigateByUrl('/admin/logga-in');
+      },
+      error: (err) => {
+        console.error('Logout error:', err.error?.message);
+      }
+    });
+  }
+}
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -7,13 +7,19 @@ import { Observable } from 'rxjs';
 })
 export class Auth {
   private readonly http = inject(HttpClient);
-  private readonly apiUrl = "http://localhost:5000/api/v1/auth/admin/login";
+  private readonly loginUrl = "http://localhost:5000/api/v1/auth/admin/login";
+  private readonly logoutUrl = "http://localhost:5000/api/v1/auth/admin/logout";
 
   loginAdmin(payload: { email: string; password: string }): Observable<any> {
-    return this.http.post(this.apiUrl, payload, {
+    return this.http.post(this.loginUrl, payload, {
       withCredentials: true
-
     })
 
+  }
+
+  logoutAdmin(): Observable<any> {
+    return this.http.post(this.logoutUrl,{}, {
+      withCredentials: true
+    })
   }
 }
