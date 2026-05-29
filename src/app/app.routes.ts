5b3799c7118247f7c1354d4070e3fa3abbdf3de4
ziba <ziba2500@student.miun.se>
2026-05-29 12:40:28 +0200

    Redirect root route to admin login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { LoggaIn } from './admin/logga-in/logga-in';
 
 export const routes: Routes = [
     {
+        path: "",
+        redirectTo: "admin/logga-in",
+        pathMatch: "full"
+    },
+    {
         path: "admin/logga-in",
         component: LoggaIn,
         title: "Admin | Logga in"
